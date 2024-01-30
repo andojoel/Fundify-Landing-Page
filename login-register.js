20589de737c7c4a0c649cda9a0f5eb110da71599
Yukislayers <anthony.kau96@gmail.com>
2024-01-30 16:37:49 +0000

    Added files from dev-ando branch

---
+++ login-register.js
@@ -0,0 +1,28 @@
+async function fetchData(link, payload){
+    try{
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+
+        request = await fetch(link, {
+            method: "POST",
+            mode:"cors",
+            headers: headers,
+            body: payload
+        });
+        if(!(request.Accept || request.ok)){
+            throw new Error("Une erreur est survenue.", {
+                cause: {request}
+        })}
+        else return request.json();
+    }
+    catch(err){
+        throw err.cause.request;
+    }
+}
+
+function displayError(text) {
+    const toast = document.getElementById('toast');
+    toast.textContent = text;
+    toast.style.display = "block";
+}
