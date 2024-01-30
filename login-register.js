df4a5b4148e3a1f4d1c79a9a891ca7ec4d440b87
Ando <ando2joel@gmail.com>
2024-01-30 12:44:32 +0000

    Fix : factorisation du code de display d'erreur

---
+++ login-register.js
@@ -0,0 +1,35 @@
+async function fetchData(link, payload){
+    try{
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+
+        request = await fetch(link, {
+            method: "POST",
+            mode:"cors",
+            headers: headers,
+            body: payload
+        });
+        if(!(request.Accept || request.ok)){
+            throw new Error("Tentative de connexion échoué", {
+            cause: {request}
+        })}
+        return request.json();
+    }
+    catch(err){
+        switch (err.cause.request?.status){
+            case 401: 
+                displayError("Tentative de connexion échoué. Identifiants non valides"); 
+                break;
+            default: displayError(err)
+        }
+        throw err;
+    }
+}
+
+function displayError(text) {
+    console.log(text);
+    const toast = document.getElementById('toast');
+    toast.textContent = text;
+    toast.style.display = "block";
+}
