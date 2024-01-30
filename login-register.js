79af6e1d403de269ecd2bc479d18ba80981adef1
Ando <ando2joel@gmail.com>
2024-01-30 14:28:43 +0000

    Fix

--- login-register.js
+++ login-register.js
@@ -17,7 +17,7 @@ async function fetchData(link, payload){
         else return request.json();
     }
     catch(err){
-        console.log(request.json());
+        console.log(request.clone().json());
         throw err;
     }
 }
